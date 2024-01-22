f7f4e90806192608afd902e4cdb98d42776fa3d9
Alalidus <157357443+Alalidus@users.noreply.github.com>
2024-01-22 11:21:46 +0100

    Update script.bat

--- script.bat
+++ script.bat
@@ -6,7 +6,7 @@ set passwrd=123
 :parameters
 	if /I %~1 == -file SET file=%2& SHIFT
 	if /I %~1 == -pswd SET passwrd=%2& SHIFT
-	if /I %~1 == -help goto help shift
+	if /I %~1 == -help goto napoveda shift
 	SHIFT
 
 if not (%1)==() goto parameters
@@ -17,11 +17,11 @@ FOR /F "tokens=*" %%i IN (%file%) DO call :vytvoreni %%i %passwrd%
 :vytvoreni
 net user %1 /fullname:"%1 %2" %3 /add
 
-goto END
+goto konec
 
-:help
+:napoveda
 echo help:
 echo -file --cesta k souboru
 echo -pswd --heslo k uctu
 
-:END
+:konec
