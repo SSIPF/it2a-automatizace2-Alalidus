f864cb9f140a81e962ee49f6c542fb83fea6afb3
Alalidus <157357443+Alalidus@users.noreply.github.com>
2024-02-18 16:12:13 +0100

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,20 +1,47 @@
 @echo off
+setlocal
 
-set file=<User_list.txt>
-set passwrd=123
+set file="User_list.txt"
+set passwrd="123"
 
 :parameters
-	if /I %~1 == -file SET file=%2& SHIFT
-	if /I %~1 == -pswd SET passwrd=%2& SHIFT
-	if /I %~1 == -help goto napoveda shift
+	if /I %~1 == "-file" SET file="%2"& SHIFT
+	if /I %~1 == "-pswd" SET passwrd="%2"& SHIFT
+	if /I %~1 == "-help" goto napoveda shift
 	SHIFT
 
-if not (%1)==() goto parameters
+if not "%1"=="" goto parameters
 
-FOR /F "tokens=*" %%i IN (%file%) DO call :vytvoreni %%i %passwrd%
+if not exist %file% (
+	echo File %file% not found.
+	pause
+	exit /b 1
+)
+
+FOR /F "tokens=*" %%i IN (%file%) DO call :vytvoreni %%~i "%passwrd%"
+
+goto END
 
 :vytvoreni
-net user %1 /fullname:"%1 %2" %3 /add
+pushd %~dp0
+net user "%~1" /fullname:"%~1 %~2" "%~3" /add
+ef errorlevel 1 (
+	echo Failed to create user account %~1.
+	pause
+	exit /b 2
+) else (
+	echo User account %~1 created successfully.
+)
+popd
+
+got :EOF
+
+:napoveda
+echo help:
+echo -file --cesta k souboru
+echo -pswd --heslo k uctu
+
+:END
 
 goto END
 
